e3a102c55a7579f2770ccb1bc80ba596253d82a4
FionaYang <yny@umich.edu>
2014-04-05 17:27:10 -0400

    Some view controller added

---
+++ Werewolves/Werewolves/Model/TurnWerewolfViewController.h
@@ -0,0 +1,13 @@
+//
+//  TurnWerewolfViewController.h
+//  Werewolves
+//
+//  Created by Fiona Yang on 4/5/14.
+//  Copyright (c) 2014 GoGo-KiWi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TurnWerewolfViewController : UIViewController
+
+@end
---
+++ Werewolves/Werewolves/Model/TurnWitchSaveViewController.h
@@ -0,0 +1,16 @@
+//
+//  TurnWitchSaveViewController.h
+//  Werewolves
+//
+//  Created by Fiona Yang on 4/5/14.
+//  Copyright (c) 2014 GoGo-KiWi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TurnWitchSaveViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *playerKilledInfo;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *saveInfo;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@end
