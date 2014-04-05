bfdfc4da98de9454f15179584ec0e16d228b57f7
Gloria Xu <gloriaxu@Glorias-MacBook-Pro.local>
2014-04-05 17:21:34 -0400

    TableView method added

---
+++ Werewolves/Werewolves/AssignRoleViewController.h
@@ -0,0 +1,14 @@
+//
+//  AssignRoleViewController.h
+//  Werewolves
+//
+//  Created by Gloria Xu on 4/5/14.
+//  Copyright (c) 2014 GoGo-KiWi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssignRoleViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *roleTableView;
+
+@end
---
+++ Werewolves/Werewolves/AssignRoleViewController.m
@@ -0,0 +1,101 @@
+//
+//  AssignRoleViewController.m
+//  Werewolves
+//
+//  Created by Gloria Xu on 4/5/14.
+//  Copyright (c) 2014 GoGo-KiWi. All rights reserved.
+//
+
+#import "AssignRoleViewController.h"
+
+@interface AssignRoleViewController ()
+
+@end
+
+@implementation AssignRoleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 4;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return 3;
+            break;
+        case 1:
+            return 3;
+        case 2:
+            return 1;
+        case 3:
+            return 1;
+        default:
+            break;
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc]init];
+    NSString *headerTitle = [self tableView:tableView titleForHeaderInSection:indexPath.section];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ %ld", headerTitle, (long)[indexPath row]];
+    return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return @"Peasant";
+            break;
+        case 1:
+            return @"Werewolf";
+            break;
+        case 2:
+            return @"Fortune Teller";
+            break;
+        case 3:
+            return @"Witch";
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+@end
